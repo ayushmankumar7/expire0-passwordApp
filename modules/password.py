1c33865bee6af79518c06a89382e3071afe38ca1
ayushmankumar7 <ayushmankumar7@gmail.com>
2019-10-13 16:27:22 +0530

    Password Generator Updated

--- modules/password.py
+++ modules/password.py
@@ -1,17 +1,42 @@
-import random
+from os import urandom
+from random import choice
 
-def password_generator():
-    a = [chr(i) for i in range(97, 122)]  #lower_case
-    A = [chr(i) for i in range(65, 91)]   #Upper_case
-    one = [i  for i in range(10)]           # Integers
-    sc = ['~','!','@','#','$','%','^','*','_','-','+','=','`','|','<','>',  '?',  '/'] #symbols
-    p = [a,A,one,sc]
-    password = ""
-    for _ in range(24):
-        a =random.randint(0,len(p) -1 )
-        
-        password += str((p[a][random.randint(0,len(p[a])- 1)] ))
+char_set = {'small': 'abcdefghijklmnopqrstuvwxyz',
+             'nums': '0123456789',
+             'big': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+             'special': '^!\$%&/()=?{[]}+~#-_.:,;<>|\\'
+            }
 
-    return password
 
-print(password_generator())
+def generate_pass(length=21):
+    """Function to generate a password"""
+
+    password = []
+
+    while len(password) < length:
+        key = choice(char_set.keys())
+        a_char = urandom(1)
+        if a_char in char_set[key]:
+            if check_prev_char(password, char_set[key]):
+                continue
+            else:
+                password.append(a_char)
+    return ''.join(password)
+
+
+def check_prev_char(password, current_char_set):
+    """Function to ensure that there are no consecutive 
+    UPPERCASE/lowercase/numbers/special-characters."""
+
+    index = len(password)
+    if index == 0:
+        return False
+    else:
+        prev_char = password[index - 1]
+        if prev_char in current_char_set:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    print generate_pass()
